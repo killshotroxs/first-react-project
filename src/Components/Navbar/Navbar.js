e99eb03a65dc9478215fa2e6570242c1bf5452b0
Sandhra Rahardjo <ailhardaisy@gmail.com>
2023-08-08 15:48:10 +0700

    fix-0003 add components folder

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Nav, NavLink, Bars,NavMenu} from './Navbar.styles';  
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/Postings' activeStyle>
+            Postings
+          </NavLink>
+          <NavLink to='/Profile' activeStyle>
+            Profile
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+    
+export default Navbar;
